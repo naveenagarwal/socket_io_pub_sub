f5d34dfb0877a5b854e2da2395817c2b306fc461
Naveen <Naveen@idyllic-software.com>
2013-02-14 01:15:33 +0530

    node scket io express app for pub sub functionality

---
+++ app.js
@@ -0,0 +1,79 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , http    = require('http')
+  , routes  = require('./routes');
+
+var app = module.exports = express.createServer();
+var io =  require('socket.io').listen(app);
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+app.post('/feed', function(request, response){
+  
+
+  var payload_id = request.body.payload;
+  var reuestEnv = request.body.request_env;
+
+  if(reuestEnv == "development"){
+    
+    feedURL = "http://localhost:3000/get_feed?activity=" + payload_id;
+
+  }else if(reuestEnv == "staging"){
+    
+    feedURL = "http://localhost:3000/get_feed?activity=" + payload_id;
+  
+  }else if(reuestEnv == "production"){
+    
+    feedURL = "http://localhost:3000/get_feed?activity=" + payload_id;
+  
+  }
+
+  http.get(feedURL, function(res){
+    
+    res.on('data', function (chunk) {
+      var payloadData = JSON.parse(chunk);
+
+      var channels = request.body.channels;
+      var i;
+
+      for(i = 0; i < channels.length; i++){
+        io.sockets.emit(channels[i], { payload: payloadData["html_data"] });
+      }
+
+    });
+    
+  })
+
+
+  response.send(200);
+
+});
+
+
+app.listen(4444, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
