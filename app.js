e642245adcda2ed2c6f548e8b4f2bc06acba388e
Naveen <Naveen@idyllic-software.com>
2013-02-14 20:37:31 +0530

    staging feed url chhanged

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.post('/feed', function(request, response){
 
   }else if(reuestEnv == "staging"){
     
-    feedURL = "http://184.72.146.82:4444/get_feed?activity=" + payload_id;
+    feedURL = "http://mte.idyllic-software.com/get_feed?activity=" + payload_id;
   
   }else if(reuestEnv == "production"){
     
